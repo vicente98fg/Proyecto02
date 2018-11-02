140dffee0718863da36363a8be140778049f0954
vicente98fg <vicente98fg@gmail.com>
2018-11-02 21:33:27 +0100

    Proyecto02

---
+++ proyecto02.js
@@ -0,0 +1,16 @@
+
+
+//Zona de funciones
+
+function iniciaTablero (numero,color) {
+    return numero;
+}
+
+var numero = Array(1, 2, 3, 4, 5, 6);
+var color = Array("rojo", "azul", "verde", "amarillo", "rosa", "blanco");
+
+
+function compruebaTablero (numero,color) {
+    console.log(numero);
+    console.log(color);
+}
